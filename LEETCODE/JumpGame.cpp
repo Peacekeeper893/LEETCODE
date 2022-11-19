111ae34ee1ac027dbc617cba9b27462f9d876dae
Navneet Sindhwani <navneetsindhwani009@gmail.com>
2022-11-19 16:54:13 +0530

    better code committed

--- LEETCODE/JumpGame.cpp
+++ LEETCODE/JumpGame.cpp
@@ -27,3 +27,26 @@ public:
         return dp[n - 2];
     }
 };
+
+/*
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n = nums.size();
+        int reach = 0;
+
+        int curr_end = 0;
+        for(int i =  0;i<n;i++)
+        {
+            reach = max(reach , nums[i]+i);
+
+            if(i == curr_end)
+                curr_end = reach;
+        }
+
+        return curr_end>=nums.size()-1;
+        
+    }
+};
+*/
